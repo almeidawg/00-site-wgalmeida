ae07f798428d6c125a5642174d6871d91379f535
William Almeida <william@wgalmeida.com.br>
2026-09-16 00:58:25 -0300

    fix(identity): align William article author entity

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -247,13 +247,13 @@ export function SEO({
 export default SEO;
 
 const WILLIAM_ALMEIDA = {
+  '@id': `${BASE_URL}/william-almeida#person`,
   '@type': 'Person',
   name: 'William Almeida',
-  jobTitle: 'Arquiteto e Fundador',
-  url: 'https://wgalmeida.com.br/sobre',
+  jobTitle: 'Founder, Operator & Builder | Advisor Estratégico',
+  url: 'https://wgalmeida.com.br/william-almeida',
   sameAs: [
-    'https://www.linkedin.com/in/william-almeida-wg',
-    'https://www.behance.net/wgalmeida'
+    'https://www.linkedin.com/in/wgalmeida/'
   ],
   image: 'https://wgalmeida.com.br/images/about/william-almeida-1200.webp'
 };
